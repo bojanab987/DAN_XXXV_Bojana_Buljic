d9353e4ae228708d869af0d988aa5186de6ab884
bojanabb <bojana.buljic@gmail.com>
2020-06-19 14:49:13 +0200

    Added GenerateGuessingNo Method

--- DAN_XXXV_Bojana_Buljic/Zadatak_1/Program.cs
+++ DAN_XXXV_Bojana_Buljic/Zadatak_1/Program.cs
@@ -8,6 +8,30 @@ namespace Zadatak_1
 {
     class Program
     {
+        static int users;
+        static int number;       
+        static Random rnd = new Random();
+
+        /// <summary>
+        /// Static method for Generating random number for guesing and asking for user's input about number of persons to play
+        /// </summary>
+        static void GenerateGuessingNo()
+        {
+            Console.WriteLine("How many persons will be guessing the number?");
+            bool success = int.TryParse(Console.ReadLine(), out users);
+            while (!success || users <= 0)
+            {
+                Console.WriteLine("Input is not valid. You need to enter number greater than 0. Please try again.");
+                success = int.TryParse(Console.ReadLine(), out users);
+            }
+            Console.WriteLine("Generating number...\n");
+            number = rnd.Next(1, 101);            
+
+            Console.WriteLine("You selected {0} persons to guess the number.", users);
+            Console.WriteLine("Number for guessing is generated!\nYou can start guessing.");
+
+        }
+
         static void Main(string[] args)
         {
         }
