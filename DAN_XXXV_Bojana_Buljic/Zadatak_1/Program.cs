eab76c1b241c141ed17f087765a6cfc324dc3ad4
bojanabb <bojana.buljic@gmail.com>
2020-06-19 15:40:08 +0200

    secondThread instance created and called inside of first method

--- DAN_XXXV_Bojana_Buljic/Zadatak_1/Program.cs
+++ DAN_XXXV_Bojana_Buljic/Zadatak_1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Zadatak_1
@@ -11,6 +12,7 @@ namespace Zadatak_1
         static int users;
         static int number;       
         static Random rnd = new Random();
+        static Thread secondThread;
 
         /// <summary>
         /// Static method for Generating random number for guesing and asking for user's input about number of persons to play
@@ -25,8 +27,9 @@ namespace Zadatak_1
                 success = int.TryParse(Console.ReadLine(), out users);
             }
             Console.WriteLine("Generating number...\n");
-            number = rnd.Next(1, 101);            
-
+            number = rnd.Next(1, 101);
+            secondThread = new Thread(new ThreadStart(ThreadGenerator));
+            secondThread.Start();
             Console.WriteLine("You selected {0} persons to guess the number.", users);
             Console.WriteLine("Number for guessing is generated!\nYou can start guessing.");
 
@@ -34,6 +37,9 @@ namespace Zadatak_1
 
         static void Main(string[] args)
         {
+            Thread firstThread = new Thread(new ThreadStart(GenerateGuessingNo));
+            firstThread.Start();
+            firstThread.Join();
         }
     }
 }
